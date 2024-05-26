a55efdb73ffa403b04a35e494a044b3a783dd299
Baligh Mnassri <33178124+mnassrib@users.noreply.github.com>
2024-05-26 02:39:46 +0200

    Add files via upload
    projet base de données SuperStore sur docker

---
+++ README.md
@@ -0,0 +1,101 @@
+# Projet ETL avec Docker, PostgreSQL, pgAdmin et JupyterLab
+
+Ce projet propose une configuration Docker pour exécuter un processus ETL (Extract, Transform, Load) sur des données de vente de la base de données SuperStore. Le processus ETL extrait les données d'un fichier CSV, les transforme selon les besoins et les charge dans une base de données PostgreSQL. Il utilise également pgAdmin pour gérer la base de données via une interface web et JupyterLab pour explorer et visualiser les données.
+
+## Prérequis
+
+- Docker installé sur votre système. [Guide d'installation Docker](https://docs.docker.com/get-docker/)
+- Un navigateur web pour accéder aux interfaces utilisateur (pgAdmin et JupyterLab).
+
+## Structure du Répertoire
+
+```
+superstore-project/
+├── data/
+│   └── SuperStoreData.csv
+├── etl/
+│   ├── Dockerfile
+│   ├── etl.py
+│   └── requirments.txt
+├── images/
+│   └── erd.png
+├── notebooks/
+├── docker-compose.yml
+└── README.md
+```
+
+## Configuration
+
+1. Clonez ce dépôt sur votre machine :
+
+    ```bash
+    git clone https://github.com/mnassrib/superstore-project.git
+    cd superstore-project
+    ```
+
+2. Ouvrez un terminal dans le répertoire du projet et exécutez la commande suivante pour démarrer les services Docker :
+
+    ```bash
+    docker-compose up --build
+    ```
+
+3. Une fois les conteneurs démarrés, vous pourrez accéder aux services suivants dans votre navigateur web :
+
+    - **pgAdmin** : [http://localhost:8080](http://localhost:8080)
+    - **JupyterLab** : [http://localhost:8888](http://localhost:8888)
+
+## Contenu du Projet
+
+- **data/** : Contient le fichier CSV contenant les données de vente.
+- **docker-compose.yml** : Fichier de configuration Docker définissant les services et leurs paramètres.
+- **etl/** : Contient le script ETL et son Dockerfile.
+    - `Dockerfile` : Fichier Dockerfile pour construire l'image Docker du service ETL.
+    - `etl.py` : Script Python exécutant le processus ETL.
+    - `requirments.txt` : Fichier texte pour spécifier les dépendances et les packages Python nécessaires au projet.
+- **images/** : Contient les images utilisées dans le README.md.
+    - `erd.png` : Entity Relationship Diagram (ERD) illustrant la structure des tables de la base de données SuperStore.
+- **notebooks/** : Pour contenir les fichiers notebooks pour les analyses et visualisations des données. 
+- **README.md** : Ce fichier, fournissant des instructions sur la configuration et l'utilisation du projet.
+
+## Utilisation
+
+1. Connectez-vous à pgAdmin avec les informations d'identification suivantes :
+    - Email : `admin@admin.com`
+    - Mot de passe : `admin`
+    - Créer un nouveau serveur :
+        - Clic droit sur "Servers" > "Create" > "Server..."
+    - Configurer le serveur :
+        - Onglet "General" : Name = SuperStoreDB
+    - Onglet "Connection" :
+        - Host name/address = db
+        - Port = 5432
+        - Maintenance database = SuperStoreDB
+        - Username = postgres
+        - Password = postgres
+    - Cliquez sur "Save"
+
+2. Utilisez pgAdmin pour explorer, interroger et gérer la base de données PostgreSQL.
+
+3. Connectez-vous à JupyterLab avec le token d'accès affiché dans la console au démarrage.
+
+4. Utilisez JupyterLab pour explorer, analyser et visualiser les données à l'aide de notebooks Python.
+
+## Auteurs
+
+- [B. Mnassri](https://github.com/mnassrib) - Développeur principal
+
+## Note
+
+Les données utilisées dans ce projet, ainsi que la normalisation des tables de la base de données, sont inspirées de l'article intitulé "[DATABASE DESIGN: BUILDING A BUSINESS DATABASE FROM A CSV FILE](https://medium.com/@oluwatobiaina/database-design-building-a-business-database-from-a-csv-file-5698e87b1e78)" rédigé par l'auteur O. Aina. Cet article offre une approche détaillée pour concevoir une base de données à partir d'un fichier CSV, ce qui a servi de base pour la conception de la structure de la base de données SuperStore dans ce projet.
+
+## Entity Relationship Diagram (ERD)
+
+L'Entity Relationship Diagram (ERD) ci-dessous illustre la structure des tables de la base de données SuperStore :
+
+![ERD](images/erd.png)
+
+- **orders** : Table principale contenant les informations sur les commandes.
+- **customers** : Table contenant les informations sur les clients.
+- **product** : Table contenant les informations sur les produits.
+- **sales_team** : Table contenant les informations sur l'équipe de vente.
+- **location** : Table contenant les informations sur les emplacements.
---
+++ etl/etl.py
@@ -0,0 +1,194 @@
+import psycopg2
+import os
+
+# Lire les variables d'environnement
+db_host = os.getenv('DB_HOST', 'db')
+db_name = os.getenv('DB_NAME', 'SuperStoreDB')
+db_user = os.getenv('DB_USER', 'postgres')
+db_password = os.getenv('DB_PASSWORD', 'postgres')
+csv_file_path = '/data/SuperStoreData.csv'  # Mettez à jour le chemin du fichier CSV si nécessaire
+
+try:
+    # Connexion initiale pour créer la base de données
+    conn = psycopg2.connect(dbname='postgres', user=db_user, password=db_password, host=db_host)
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    # Suppression et création de la base de données
+    cur.execute('DROP DATABASE IF EXISTS "SuperStoreDB"')
+    print("Base de données supprimée")
+    cur.execute('CREATE DATABASE "SuperStoreDB"')
+    print("Base de données créée")
+    cur.close()
+    conn.close()
+
+    # Connexion à la nouvelle base de données
+    conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_password, host=db_host)
+    cur = conn.cursor()
+
+    # Configuration du datestyle
+    cur.execute("SET datestyle = 'MDY';")
+
+    # Suppression et création de la table orders
+    cur.execute('''
+    DROP TABLE IF EXISTS orders;
+    CREATE TABLE orders(
+        order_id CHAR(14),
+        order_date DATE,
+        ship_date DATE,
+        ship_mode VARCHAR(100),
+        customer_id CHAR(8),
+        customer_name VARCHAR(50),
+        segment VARCHAR(100),
+        sales_rep VARCHAR(100),
+        sales_team VARCHAR(100),
+        sales_team_manager VARCHAR(100),
+        location_id VARCHAR(225),
+        city VARCHAR(100),
+        state VARCHAR(100),
+        postal_code CHAR(5),
+        region VARCHAR(100),
+        product_id CHAR(15),
+        category VARCHAR(225),
+        sub_category VARCHAR(225),
+        product_name VARCHAR(225),
+        sales DECIMAL (10,2),
+        quantity INT,
+        discount DECIMAL(10,2),
+        profit DECIMAL(10, 2)
+    );
+    ''')
+    print("Table orders créée")
+    conn.commit()
+
+    # Copie des données depuis le fichier CSV
+    with open(csv_file_path, 'r', encoding='utf-8') as f:
+        next(f)  # Skip the header row
+        cur.copy_expert("COPY orders FROM STDIN WITH CSV DELIMITER ','", f)
+    conn.commit()
+    print("Données importées dans la table orders")
+
+    # Début de la transaction
+    cur.execute('BEGIN;')
+
+    # Création des tables dérivées
+    cur.execute('''
+    DROP TABLE IF EXISTS customers;
+    CREATE TABLE customers AS
+    SELECT DISTINCT customer_id, customer_name, segment FROM orders;
+    ''')
+    print("Table customers créée")
+
+    cur.execute('''
+    DROP TABLE IF EXISTS product;
+    CREATE TABLE product AS
+    SELECT DISTINCT product_id, product_name, category, sub_category FROM orders;
+    ''')
+    print("Table product créée")
+
+    cur.execute('''
+    DROP TABLE IF EXISTS sales_team;
+    CREATE TABLE sales_team AS
+    SELECT DISTINCT sales_rep, sales_team, sales_team_manager FROM orders;
+    ''')
+    print("Table sales_team créée")
+
+    cur.execute('''
+    DROP TABLE IF EXISTS location;
+    CREATE TABLE location AS
+    SELECT DISTINCT location_id, city, state, postal_code, region FROM orders;
+    ''')
+    print("Table location créée")
+    conn.commit()
+
+    # Suppression des colonnes redondantes de la table orders
+    cur.execute('''
+    ALTER TABLE orders
+    DROP COLUMN customer_name,
+    DROP COLUMN segment,
+    DROP COLUMN sales_team,
+    DROP COLUMN sales_team_manager,
+    DROP COLUMN city,
+    DROP COLUMN state,
+    DROP COLUMN postal_code,
+    DROP COLUMN region,
+    DROP COLUMN category,
+    DROP COLUMN sub_category,
+    DROP COLUMN product_name;
+    ''')
+    conn.commit()
+    print("Colonnes redondantes supprimées de la table orders")
+
+    # Ajout de clés primaires et de contraintes de clé étrangère
+    cur.execute('''
+    ALTER TABLE customers
+    ADD CONSTRAINT customers_id PRIMARY KEY (customer_id),
+    ALTER COLUMN customer_id SET NOT NULL;
+    ''')
+
+    cur.execute('''
+    ALTER TABLE location
+    ADD CONSTRAINT location_id PRIMARY KEY (location_id),
+    ALTER COLUMN location_id SET NOT NULL;
+    ''')
+
+    cur.execute('''
+    ALTER TABLE product
+    ADD CONSTRAINT product_id PRIMARY KEY (product_id),
+    ALTER COLUMN product_id SET NOT NULL;
+    ''')
+
+    cur.execute('''
+    ALTER TABLE sales_team
+    ADD CONSTRAINT sales_rep_pk PRIMARY KEY (sales_rep),
+    ALTER COLUMN sales_rep SET NOT NULL;
+    ''')
+
+    cur.execute('''
+    ALTER TABLE orders
+    ADD COLUMN order_serial_id SERIAL PRIMARY KEY,
+    ADD CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customers (customer_id),
+    ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES product (product_id),
+    ADD CONSTRAINT fk_location_id FOREIGN KEY (location_id) REFERENCES location (location_id),
+    ADD CONSTRAINT fk_sales_rep FOREIGN KEY (sales_rep) REFERENCES sales_team (sales_rep);
+    ''')
+    conn.commit()
+    print("Contraintes ajoutées")
+
+    # Création d'index pour améliorer les performances
+    cur.execute('''
+    CREATE INDEX idx_orders_customer_id ON orders(customer_id);
+    CREATE INDEX idx_orders_product_id ON orders(product_id);
+    CREATE INDEX idx_orders_location_id ON orders(location_id);
+    CREATE INDEX idx_orders_sales_rep ON orders(sales_rep);
+    ''')
+    conn.commit()
+    print("Index créés")
+
+    # Suppression et création de la vue customer_category
+    cur.execute('''
+    DROP VIEW IF EXISTS customer_category;
+    CREATE VIEW customer_category AS
+    SELECT 
+        c.customer_name,
+        COALESCE(SUM(o.sales * o.quantity), 0) AS amount_spent,
+        CASE
+            WHEN COALESCE(SUM(o.sales * o.quantity), 0) < 5000 THEN 'Silver Customer'
+            WHEN COALESCE(SUM(o.sales * o.quantity), 0) <= 10000 THEN 'Gold Customer'
+            ELSE 'Diamond Customer'
+    END AS customer_category
+    FROM orders AS o
+    LEFT JOIN customers AS c USING(customer_id)
+    GROUP BY c.customer_name
+    ORDER BY amount_spent DESC;
+    ''')
+    conn.commit()
+    print("Vue customer_category créée")
+
+finally:
+    # Fermeture de la connexion
+    if cur:
+        cur.close()
+    if conn:
+        conn.close()
+    print("Connexion fermée")
